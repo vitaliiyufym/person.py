335efc636a77066963dcdc31c26ca6963361acd1
vitaliiyufym <vitalii.yufym.kb.2025@lpnu.ua>
2025-09-26 10:24:39 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+formula1 = "s = s0 + v0*t + (1/2)*g*t**2"
+print("Шлях:")
+print(formula1)
+formula2 = "FV = PV * (1 + INT/100)**YRS"
+print("\n2.Вартость (FV):")
+print(formula2)
+formula3 = "G = 4 * math.pi**2 * a**3 / (p**2 * (m1 + m2))"
+print("\n3.Гравітація (G):")
+print(formula3)
+formula4 = "c = math.sqrt(a**2 + b**2 - 2*a*b * math.cos(gamma))"
+print("\n4.Косинус:")
+print(formula4)
